73ec4f511844385b6846ec5f0e5a1c8b419a4604
Lauren <mlmxwell@gmail.com>
2022-03-08 18:07:59 -0800

    Update Header.js
    Update to latest version of course

--- 08-higher-order-components-and-render-props/clip-06-using-hoc-in-components/src/components/Header.js
+++ 08-higher-order-components-and-render-props/clip-06-using-hoc-in-components/src/components/Header.js
@@ -4,6 +4,30 @@ import withAuth from "./withAuth";
 
 function Header({ loggedInUser, setLoggedInUser }) {
   const { theme } = useContext(ThemeContext);
+  function LoggedIn({ loggedInUser, setLoggedInUser }) {
+    return (
+      <div>
+        <span>Logged in as {loggedInUser} </span>
+        <button
+          className="btn btn-secondary"
+          onClick={() => {
+            setLoggedInUser("");
+          }}>Logout</button>
+      </div>
+    )
+  }
+
+  function LoggedOut({ setLoggedInUser }) {
+    return (
+      <button
+        className="btn btn-secondary"
+        onClick={(e) => {
+          e.preventDefault();
+          const username = window.prompt("Enter Login name:", "");
+          setLoggedInUser(username);
+        }}>Login</button>
+    )
+  }
 
   return (
     <div className="padT4 padB4">
@@ -16,30 +40,10 @@ function Header({ loggedInUser, setLoggedInUser }) {
             <h4 className="header-title">Silicon Valley Code Camp</h4>
           </div>
           <div className={theme === "light" ? "" : "text-info"}>
-            {loggedInUser && loggedInUser.length > 0 ? (
-              <div>
-                <span>Logged in as {loggedInUser} </span>
-                <button
-                  className="btn btn-secondary"
-                  onClick={() => {
-                    setLoggedInUser("");
-                  }}
-                >
-                  Logout
-                </button>
-              </div>
-            ) : (
-              <button
-                className="btn btn-secondary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  const username = window.prompt("Enter Login name:", "");
-                  setLoggedInUser(username);
-                }}
-              >
-                login
-              </button>
-            )}
+            {loggedInUser && loggedInUser.length > 0 ? 
+              <LoggedIn loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />:
+              <LoggedOut setLoggedInUser={setLoggedInUser} />
+            }
           </div>
         </div>
       </div>
